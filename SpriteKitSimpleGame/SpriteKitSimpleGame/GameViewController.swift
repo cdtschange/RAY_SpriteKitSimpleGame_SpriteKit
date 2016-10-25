d266f7475de9cb677345b6a8d021834ccb129545
wei.mao <wei.mao@jimubox.com>
2016-10-25 14:57:14 +0800

    SpriteKit Swift 3 Tutorial for Beginners

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Mao on 25/10/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
